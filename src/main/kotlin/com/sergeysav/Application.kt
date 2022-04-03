c3bf5b92a1d33a4d733538c13285bd4eb5d2d2e4
Sergey Savelyev <sergeysav.nn@gmail.com>
2022-04-03 13:51:23 -0700

    adds CNAME file to generator

--- src/main/kotlin/com/sergeysav/Application.kt
+++ src/main/kotlin/com/sergeysav/Application.kt
@@ -1,10 +1,15 @@
 package com.sergeysav
 
 import com.sergeysav.pages.AboutMe
+import com.sergeysav.websitegenerator.StaticResource
 import com.sergeysav.websitegenerator.WebsiteGenerator
 
 fun main(args: Array<String>) {
     WebsiteGenerator.run(args) {
+        // Add all resources related to the About Me page
         getResourcePath(AboutMe)
+
+        // Add the CNAME file (necessary for GitHub Pages)
+        getResourcePath(StaticResource("CNAME"))
     }
 }
